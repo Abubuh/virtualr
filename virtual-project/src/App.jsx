c8622b0caef900ad4dbc3820daa226455a93210e
abubuh <cristian_caht@hotmail.com>
2024-12-16 02:51:10 -0600

    Initial commit

---
+++ virtual-project/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <>
+     <h1 className="text-3xl font-bold underline bg-slate-600 h-[100vh]">
+        <Navbar></Navbar>
+    </h1>
+    </>
+  )
+}
+
+export default App
