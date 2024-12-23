01d699957d3ad8012b4d24cf9f7133bea41bbf8a
abubuh <cristian_caht@hotmail.com>
2024-12-23 04:57:53 -0600

    Add components to display, add tailwind

--- virtual-project/src/App.jsx
+++ virtual-project/src/App.jsx
@@ -1,11 +1,24 @@
-import { useState } from 'react'
+import FeatureSection from './components/FeatureSection'
+import Footer from './components/Footer'
+import HeroSection from './components/HeroSection'
 import Navbar from './components/Navbar'
+import Pricing from './components/Pricing'
+import Testimonials from './components/Testimonials'
+import WorkFlow from './components/WorkFlow'
 
 function App() {
   return (
     <>
-     <h1 className="text-3xl font-bold underline bg-slate-600 h-[100vh]">
+     <h1 className="text-3xl ">
         <Navbar></Navbar>
+        <div className="max-w-7xl mx-auto pt-20 px-6">
+          <HeroSection></HeroSection>
+          <FeatureSection></FeatureSection>
+          <WorkFlow></WorkFlow>
+          <Pricing></Pricing>
+          <Testimonials></Testimonials>
+          <Footer></Footer>
+        </div>
     </h1>
     </>
   )
